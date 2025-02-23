94c0381700b032ded1901e034d33228979e3d7f0
aramossanchez <armandoramossanchez@gmail.com>
2025-02-23 23:33:10 +0100

    fix: arreglada importacion

--- containers/home/HomeContainer.tsx
+++ containers/home/HomeContainer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import WhatsappButton from '@/components/WhatsappButton/WhatsappButton';
-import PresentationSection from './components/PresentationSection/PresentationSection';
+import PresentationSection from './components/presentationSection/PresentationSection';
 
 export default function HomeContainer() {
   return (
